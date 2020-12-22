bff262e0e72f0b5ac1384bc8ffcdd711e86fc7f3
Bram Adams <bram@othersideai.com>
2020-12-22 12:09:19 -0500

    popup menu to add blocks recording state changes in each block

--- components/completion-block.js
+++ components/completion-block.js
@@ -1,31 +1,38 @@
-export default function CompletionBlock({ id = "NYI" }) {
+export default function CompletionBlock({ id = "NYI", prompts = ['123', '4567'], onChange, completions }) {
+ 
+    // TODO add block IDs to form IDs
     return (
-        <div className="w-full">
+        <div className="w-full" onChange={() => onChange({
+            prompt: document.getElementById("prompt-select").value,
+            engine: document.getElementById("engine-select").value,
+            temperature: document.getElementById("tempRange").value
+        })}>
             <h1 className="text-2xl">Completion Block (ID = {id})</h1>
 
             <div className="flex space-x-4">
                 <div className="flex-grow box-border border-4 bg-gray-100 rounded-xl p-8">
                     <div className="inline-block px-3">
-                        <label for="Prompt" className="block">Prompt:</label>
-                        <select className="block">
-                            <option>123</option>
-                            <option>456</option>
+                        <label htmlFor="Prompt" className="block">Prompt:</label>
+                        <select className="block" id="prompt-select">
+                            {prompts.map((p, idx) => <option key={idx}>{p}</option>)}
                         </select>
                     </div>
                     <div className="inline-block px-3">
-                        <label for="engine" className="block">Engine:</label>
-                        <select name="engine" className="block">
+                        <label htmlFor="engine" className="block">Engine:</label>
+                        <select name="engine" className="block" id="engine-select">
                             <option>Ada</option>
                             <option>Babbage</option>
+                            <option>Curie</option>
+                            <option>Davinci</option>
                         </select>
                     </div>
                     <div className="inline-block px-3">
-                        <label for="temp" className="block">Temp:</label>
-                        <input type="range" name="temp" min="1" max="100" value="50" id="myRange" />
+                        <label htmlFor="temp" className="block">Temp:</label>
+                        <input type="range" name="temp" min="1" max="100" defaultValue="50" id="tempRange" />
                     </div>
                     <div className="inline-block px-3">
-                        <label for="temp" className="block">Top P:</label>
-                        <input type="range" name="temp" min="1" max="100" value="50" id="myRange" />
+                        <label htmlFor="temp" className="block">Top P:</label>
+                        <input type="range" name="temp" min="1" max="100" defaultValue="50" id="top_pRange" />
                     </div>
                 </div>
             </div>
--- pages/index.js
+++ pages/index.js
@@ -6,28 +6,50 @@ import { useState } from 'react'
 import { v4 as uuidv4 } from 'uuid';
 
 export default function Home() {
+  const [completions, setCompletions] = useState([])
 
-  const [blocks, appendBlock] = useState([<PromptBlock id={uuidv4()}/>, <CompletionBlock id={uuidv4()}/>])
-  function addBlock (type = "") {
-    if (type === "Prompt") appendBlock(blocks => [...blocks, <PromptBlock id={uuidv4()}/>])
-    else if (type === "Completion") appendBlock(blocks => [...blocks, <CompletionBlock id={uuidv4()}/>])
+  function appendCompletion (val) {
+    console.log(val)
   }
+
+  const [blocks, appendBlock] = useState([<PromptBlock id={uuidv4()} />, <CompletionBlock id={uuidv4()} onChange={appendCompletion} completions={completions} />])
+  const [isMenuOpen, setMenu] = useState(false)
+  function addBlock(type = "") {
+    // TODO add list index
+    if (type === "Prompt") appendBlock(blocks => [...blocks, <PromptBlock id={uuidv4()} />])
+    else if (type === "Completion") appendBlock(blocks => [...blocks, <CompletionBlock id={uuidv4()} onChange={appendCompletion} completions={completions} />])
+  }
+  function showMenu () {
+    setMenu(!isMenuOpen)
+  }
+
+
+  function generatePython() {
+
+  }
+
   return (
-    <div>
+    <div className="container mx-auto px-2 py-1">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="container mx-auto divide-y-1">
+      <main className="divide-y-1">
         {blocks}
       </main>
-      <button className="fixed bottom-0.5 right-3.5 bg-gray-light hover:bg-gray text-gray-darkest font-bold py-2 px-4 rounded inline-flex items-center" onClick={() => addBlock("Prompt")}>Add a Prompt Block</button>
-      <button className="fixed bottom-0.5 right-4.5 bg-gray-light hover:bg-gray text-gray-darkest font-bold py-2 px-4 rounded inline-flex items-center" onClick={() => addBlock("Completion")}>Add a Completion Block</button>
+      <div className="fixed bottom-1.5 right-1.5 z-50">
+        <div id="myDropdown" className={isMenuOpen ? "pb-2" : "hidden"}>
+          <a className="block border-b-2 border-blue-600 bg-blue-200 hover:bg-blue-500 p-3 items-center" href="#"onClick={() => addBlock("Prompt")} >Add a Prompt Block</a>
+          <a className="block border-b-2 border-blue-600 bg-blue-200 hover:bg-blue-500 p-3 items-center" href="#" onClick={() => addBlock("Completion")}>Add a Completion Block</a>
+          <a className="block  border-blue-600 bg-blue-200 hover:bg-blue-500 p-3 items-center" href="#">Generate Python Code</a>
+        </div>
+        <button className="float-right bg-blue-300 hover:bg-blue-500 text-2xl font-bold py-1 px-4 rounded-full inline-flex items-center" onClick={() => showMenu()}>+</button>
 
-      {/* <footer>
-        OthersideAI
-      </footer> */}
+      </div>
+
+      {/* <button className="z-50 fixed bottom-1.5 right-3.5 bg-gray-300  font-bold py-2 px-4 rounded inline-flex items-center" onClick={() => addBlock("Prompt")}>Add a Prompt Block</button>
+      <button className="fixed bottom-1.5 right-4.5 bg-gray-300  font-bold py-2 px-4 rounded inline-flex items-center" onClick={() => addBlock("Completion")}>Add a Completion Block</button> */}
     </div>
   )
 }
